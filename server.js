0865fc3188d707b670cb17e7652c4eaa791acdc1
Midhun2823 <132231878+Midhun2823@users.noreply.github.com>
2024-05-26 00:04:40 +0530

    Update server.js Added admin vercel url

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "https://digital-mru-frontend.vercel.app",
+      "https://digital-mru-admin.vercel.app",
     ],
     methods: ["POST", "GET"],
     credentials:true
